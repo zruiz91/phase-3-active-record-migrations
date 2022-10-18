53c4600981304e6c091707afb8e00b62d6893438
Zach Ruiz <powersbooth10@gmail.com>
2022-10-18 00:12:07 -0500

    commit tree 665500a825afcfb10cc908915526b2d7ff8f1a6a

---
+++ db/migrate/20221018042017_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+      # the id column is generated automatically for every table! no need to specify it here.
+    end
+  end
+end
